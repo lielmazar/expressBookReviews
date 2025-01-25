ce0b2d2b0aad315563fb161eb7277b96ecb0e970
lielmazar <liel.mazar@gmail.om>
2025-01-25 16:08:32 -0500

    implementing auth

--- final_project/index.js
+++ final_project/index.js
@@ -10,8 +10,43 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
+
+let users = [{userName: "admin", password: "admin"}];
+
+// Check if the user with the given username and password exists
+const authenticatedUser = (username, password) => {
+    // Filter the users array for any user with the same username and password
+    let validusers = users.filter((user) => {
+        return (user.username === username && user.password === password);
+    });
+    // Return true if any valid user is found, otherwise false
+    if (validusers.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    let username = req.body.userName;
+    let password = req.body.password;
+    if(username && password) {
+        if(authenticatedUser(username, password)) {
+            // Generate JWT access token
+            let accessToken = jwt.sign({
+                user: username
+            }, 'access', { expiresIn: 60 * 60 });
+            // Store access token and username in session
+            req.session.authorization = {
+                accessToken, username
+            }
+            return res.status(200).send("User successfully logged in");
+        } else {
+            return res.status(208).json({ message: "Invalid Login. Check username and password" })
+        }
+    } else {
+        return res.status(404).json({ message: "Error logging in" });
+    }
 });
  
 const PORT =5000;
