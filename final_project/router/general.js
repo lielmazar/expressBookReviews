0806d63d3bd24f317450a74a545cc219cef9545d
lielmazar <liel.mazar@gmail.com>
2025-01-26 06:40:37 -0500

    task 14

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -22,38 +22,66 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-    return res.status(200).json(books);
+ public_users.get('/', function (req, res) {
+    new Promise((resolve,reject) => {resolve(books)})
+    .then((result) => {
+            return res.status(200).json(result);
+    })
+    .catch((error) => {
+        return res.status(500).json({message: "Some error occurred", error: error});   
+    });
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
     const isbn = req.params.isbn;
-    return res.status(200).json(books[isbn]);
+    new Promise((resolve, reject) => {resolve(books[isbn])})
+    .then((result) => {
+        return res.status(200).json(result);
+    })
+    .catch((error) => {
+        return res.status(500).json({message: "Some error occurred", error: error});   
+    });
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
     const author = req.params.author;
-    let result = {message: `No books of ${author} found`};
-    for(let isbn in books) {
-        if(books[isbn].author == author) {
-            result = books[isbn];
+    new Promise((resolve, reject) => {
+        let result = {message: `No books of ${author} found`};
+        for(let isbn in books) {
+            if(books[isbn].author == author) {
+                result = books[isbn];
+            }
         }
-    }
-    return res.status(200).json(result);
+        resolve(result)
+    })
+    .then((result) => {
+        return res.status(200).json(result);
+    })
+    .catch((error) => {
+        return res.status(500).json({message: "Some error occurred", error: error});   
+    });
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
     const title = req.params.title;
-    let result = {message: `No books titled ${title} found`};
-    for(let isbn in books) {
-        if(books[isbn].title == title) {
-            result = books[isbn];
+    new Promise((resolve, reject) => {
+        let result = {message: `No books titled ${title} found`};
+        for(let isbn in books) {
+            if(books[isbn].title == title) {
+                result = books[isbn];
+            }
         }
-    }
-    return res.status(200).json(result);
+        resolve(result)
+    })
+    .then((result) => {
+        return res.status(200).json(result);
+    })
+    .catch((error) => {
+        return res.status(500).json({message: "Some error occurred", error: error});   
+    });
 });
 
 //  Get book review
